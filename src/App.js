47ec197b6f5d2c67a0b406f2dd2945eacf7e4079
jeevan <jeevankumar8187@gmail.com>
2020-06-08 16:38:24 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,100 @@
-import React from 'react';
+// import React, {Component} from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+const App = (props) =>{
+
+  const [personState , setPersonState] = useState({
+    persons :[
+      {name :'jeevan' , age : '23'},
+      {name :'akshitha', age:'24'},
+      {name :'sindhu', age:'23'}
+    ],
+    employee : "second element"
+  })
+
+  const onClickHandler = () =>{
+    console.log("clicked");
+    let changedName = personState.persons[0].name === "jeevan" ? "jeevan kumar" : "jeevan";
+    console.log(changedName);
+    setPersonState({...personState , 
+      persons :[
+        {name : changedName, age : '23'},
+        {name :'akshitha', age:'24'},
+        {name :'sindhu', age:'23'}
+      ]
+    }
+    );
+  }
+
+  return(
+    <div>
+    <h1  className="App ">Hello people</h1>
+    {console.log(personState)}
+    <Person name={personState.persons[0].name} age={personState.persons[0].age} 
+            onclick={onClickHandler}>
+            </Person>
+    <Person name={personState.persons[1].name} age={personState.persons[1].age}></Person>
+    <Person name={personState.persons[2].name} age={personState.persons[2].age}></Person>
     </div>
-  );
+  )
 }
+
+
+// class App extends Component {
+
+// state = {
+//   persons :[
+//     {name :'jeevan' , age : '23'},
+//     {name :'akshitha', age:'24'},
+//     {name :'sindhu', age:'23'}
+//   ]
+// }
+
+//   onClickHandler = () =>{
+//     console.log("clicked");
+//     let changedName = this.state.persons[0].name === "jeevan" ? "jeevan kumar" : "jeevan";
+//     console.log(changedName);
+//     this.setState({
+//       persons :[
+//         {name : changedName, age : '23'},
+//         {name :'akshitha', age:'24'},
+//         {name :'sindhu', age:'23'}
+//       ]
+//     }
+//     );
+//   }
+
+//   onChangeHandler = (event) => {
+//     console.log("changed");
+//     this.setState({
+//       persons :[
+//           {name : event.target.value, age : '23'},
+//           {name :'akshitha', age:'24'},
+//           {name :'sindhu', age:'23'}
+//       ]
+//     }
+
+//     )
+//   }
+
+//   render() {
+//     console.log(this.state)
+//     return (
+//     <div>
+//     <h1  className="App ">Hello people</h1>
+//     <Person name={this.state.persons[0].name} age={this.state.persons[0].age} 
+//             onclick={this.onClickHandler}
+//             onchange={this.onChangeHandler}>
+//             </Person>
+//     <Person name={this.state.persons[1].name} age={this.state.persons[1].age}></Person>
+//     <Person name={this.state.persons[2].name} age={this.state.persons[2].age}></Person>
+//     </div>
+//     );
+//   }
+// }
 
 export default App;
